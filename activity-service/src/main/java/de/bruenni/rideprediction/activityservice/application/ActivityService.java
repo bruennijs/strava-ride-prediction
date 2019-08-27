2cd3aff23d8a03c76e5e642e72049f829f1b2bc1
oliver.bruentje <oliver.bruentje@openknowledge.de>
2019-08-27 21:37:25 +0200

    Add file

---
+++ activity-service/src/main/java/de/bruenni/rideprediction/activityservice/application/ActivityService.java
@@ -0,0 +1,22 @@
+package de.bruenni.rideprediction.activityservice.application;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@ApplicationScoped
+@Path("/activity-service")
+public class ActivityService {
+
+    private static Logger LOG = LoggerFactory.getLogger(ActivityService.class);
+
+    @GET
+    @Path("/overview")
+    public Response getActivityOverview() {
+        return Response.status(200).build();
+    }
+}
