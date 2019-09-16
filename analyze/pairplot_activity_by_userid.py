860e95aa18d9aebc7eeb0dbbf82ceff664d52ff7
oliver.bruentje <oliver.bruentje@openknowledge.de>
2019-09-16 14:23:34 +0200

    Program arguments handling much safer

--- analyze/pairplot_activity_by_userid.py
+++ analyze/pairplot_activity_by_userid.py
@@ -7,6 +7,12 @@ parser = ArgumentParser("pairplot_activity_by_userid")
 parser.add_argument("--athlete_id", help="Unique strava athlete id to get activities for.", type=str)
 args = parser.parse_args()
 
+if args.athlete_id is None:
+    print("--athlete_id parameter is missing")
+    exit(1)
+
+# get activities from ActivityRepository
+
 es = Elasticsearch()
 
 print ("athlete_id={}".format(args.athlete_id))
@@ -15,7 +21,7 @@ searchBody = {
     "query": {
         "term": {
             "athlete.id_as_string": {
-                "value": "2416334"
+                "value": args.athlete_id
             }
         }
     }
@@ -26,4 +32,8 @@ params = {'_source': 'true', 'size': 1000 }
 activityHits = es.search(index="activity", body=searchBody, params=params)
 
 sources = list(map(lambda activity: activity["_source"], activityHits["hits"]["hits"]))
-print ("{}".format(sources))
+print ("{}".format(len(sources)))
+
+# Filter only with heart rate
+sourcesWithHeartrate = list(filter(lambda activity: activity["has_heartrate"], sources))
+print ("{}".format(len(sourcesWithHeartrate)))
