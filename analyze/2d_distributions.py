3b1bffa760beaa73cbf90796f670f373b754ac9c
oliver.bruentje <oliver.bruentje@openknowledge.de>
2019-10-08 21:03:05 +0200

    Fixed pairplot

--- analyze/2d_distributions.py
+++ analyze/2d_distributions.py
@@ -37,8 +37,8 @@ sb.set()
 # sb.swarmplot(x="start_date_delta", y="distance", hue="start_date_isoweekday", data=X)
 
 X.loc[:, 'average_heartrate'].mask(lambda x: pd.isna(x), other=0.0, inplace=True)
-# sb.pairplot(X, hue='start_date_delta' ,height=2.5)    # heart rate contains nan for almost all rides
-sb.pairplot(X, height=2.5)    # heart rate contains nan for almost all rides
+sb.pairplot(X, kind='scatter', diag_kind='hist' , hue='start_date_delta' ,height=2.5)    # heart rate contains nan for almost all rides
+# sb.pairplot(X, height=2.5)    # heart rate contains nan for almost all rides
 
 
 plt.show()
